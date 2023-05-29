88d79848a33d0953311102da147a3d2c88de05bf
Gpadam11 <geetikapadam1106@gmail.com>
2023-05-29 23:22:19 +0530

    planets-basic html, css, js

---
+++ try.js
@@ -0,0 +1,42 @@
+//when the each planter button is clicked it should take the user to the link provided
+var p1 = document.querySelector(".p1");
+        p1.addEventListener("click", function(){
+          window.location.href = "https://solarsystem.nasa.gov/planets/mercury/overview/";
+        });
+
+      var p2 = document.querySelector(".p2");
+        p2.addEventListener("click", function(){
+          window.location.href = "https://solarsystem.nasa.gov/planets/venus/overview/";
+        });
+
+        var p4 = document.querySelector(".p4");
+        p4.addEventListener("click", function(){
+          window.location.href = "https://solarsystem.nasa.gov/planets/mars/overview/";
+        }); 
+
+        var p3 = document.querySelector(".p3");
+        p3.addEventListener("click", function(){
+          window.location.href = "https://solarsystem.nasa.gov/planets/earth/overview/";
+        }); 
+
+        var p5 = document.querySelector(".p5");
+        p5.addEventListener("click", function(){
+          window.location.href = "https://solarsystem.nasa.gov/planets/jupiter/overview/";
+        }); 
+
+        var p6 = document.querySelector(".p6");
+        p6.addEventListener("click", function(){
+          window.location.href = "https://solarsystem.nasa.gov/planets/saturn/overview/";
+        }); 
+
+        var p8 = document.querySelector(".p8");
+        p8.addEventListener("click", function(){
+          window.location.href = "https://solarsystem.nasa.gov/planets/neptune/overview/";
+        }); 
+
+        var p7 = document.querySelector(".p7");
+        p7.addEventListener("click", function(){
+          window.location.href = "https://solarsystem.nasa.gov/planets/uranus/overview/";
+        }); 
+
+        
